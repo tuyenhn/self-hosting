a96eca38bc10d89fd9572d308344eedbc33c064d
tuyenhn <hntuyen282@gmail.com>
2025-05-28 16:17:34 +0700

    new image upgrade script

--- immich/upgrade.sh
+++ immich/upgrade.sh
@@ -1,3 +1,8 @@
-docker compose pull && docker compose up -d
+docker compose down -v
+docker compose pull             # Update to latest version of Immich
+docker compose create           # Create Docker containers for Immich apps without running them
+docker start immich_postgres    # Start Postgres server
+sleep 10                        # Wait for Postgres server to start up
+docker compose up -d            # Start remainder of Immich apps
 
 docker image prune
