c3f138b68906814a746d4c81d80cd4d4d310ac25
Tuyen Huynh <hntuyen282@gmail.com>
2025-10-25 08:28:25 +0000

    always prune unused images

--- immich/upgrade.sh
+++ immich/upgrade.sh
@@ -5,4 +5,4 @@ docker start immich_postgres    # Start Postgres server
 sleep 10                        # Wait for Postgres server to start up
 docker compose up -d            # Start remainder of Immich apps
 
-docker image prune
+docker image prune -f
